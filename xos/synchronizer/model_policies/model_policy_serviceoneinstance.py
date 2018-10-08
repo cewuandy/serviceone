583f8af6d5dcde2b23d149ca8900f6e9d0791fcb
Cheng-En Wu <laker1234562001@gmail.com>
2018-10-08 18:20:32 +0800

    modify policy

--- xos/synchronizer/model_policies/model_policy_serviceoneinstance.py
+++ xos/synchronizer/model_policies/model_policy_serviceoneinstance.py
@@ -32,8 +32,7 @@ class ServiceOneInstancePolicy(Policy):
 
     def handle_update(self, service_instance):
         compute_service = KubernetesService.objects.first()
-        compute_service_instance_class = Service.objects.get(
-            id=compute_service.id).get_service_instance_class()
+        compute_service_instance_class = Service.objects.get(id=compute_service.id).get_service_instance_class()
 
         serviceone = service_instance.owner.leaf_model
 
@@ -42,10 +41,8 @@ class ServiceOneInstancePolicy(Policy):
         image = slice.default_image
 
         name = "serviceone-%s" % service_instance.id
-        compute_service_instance = compute_service_instance_class(slice=slice,
-                                                                  owner=compute_service,
-                                                                  image=image, name=name,
-                                                                  no_sync=True)
+        compute_service_instance = compute_service_instance_class(slice=slice,owner=compute_service,image=image, name=name,no_sync=True)
+
         compute_service_instance.save()
 
         service_instance.compute_instance = compute_service_instance
