8d9f479a0fc1d4cb52302767d6ab5241cc8a1399
Cheng-En Wu <laker1234562001@gmail.com>
2018-10-13 14:58:43 +0800

    modify policy

--- xos/synchronizer/model_policies/model_policy_serviceoneinstance.py
+++ xos/synchronizer/model_policies/model_policy_serviceoneinstance.py
@@ -20,7 +20,7 @@ class ServiceOneInstancePolicy(Policy):
         compute_service = KubernetesService.objects.first()
         compute_service_instance_class = Service.objects.get(id=compute_service.id).get_service_instance_class()
         slice = Slice.objects.filter(name="serviceone")[0]
-        image = Image.objects.filter(name="cewuandy/apache2")[0]
+        image = Image.objects.filter(name="httpd")[0]
         name="serviceone-%s" % service_instance.id
         instance = compute_service_instance_class(slice=slice, owner=compute_service, image=image, name=name, no_sync=True)
         instance.save()
